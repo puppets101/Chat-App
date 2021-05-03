e0d182896c2ce85cac5777aee136671f7d9d1a0e
puppets101 <oscand92@gmail.com>
2021-05-03 14:22:03 +0200

    Removed unessesary files

--- server/server.js
+++ server/server.js
@@ -5,4 +5,6 @@ const app = express();
 
 app.use(express.static("client"));
 
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+app.listen(PORT, () =>
+  console.log(`Server is running on port http://localhost:${PORT}`)
+);

8c6f12aac704dea9da7a91c54490cb1c24e4ffc1
victorwikstrom <victor.wikstrom@icloud.com>
2021-05-04 10:06:02 +0200

    Tried to connecet frontend to socket unsucessfully

--- server/server.js
+++ server/server.js
@@ -1,9 +1,27 @@
 import express from "express";
-
-const PORT = 3000;
+import http from "http";
 const app = express();
+const PORT = 4000;
+
+const server = http.createServer(app);
+import { Server } from "socket.io";
+
+const io = new Server(server);
 
 app.use(express.static("client"));
+
+io.on("connection", (socket) => {
+  console.log("Client was connected. ID: " + socket.id);
+
+  // SEND TO ALL
+  io.emit("user-connected", socket.id);
+
+  // SEND TO ALL BUT SELF
+  socket.broadcast.emit("user-connected", socket.id);
+
+  // SEND ONLY TO SOCKET
+  socket.emit("user-return-msg", "Welcome user!");
+});
 
 app.listen(PORT, () =>
   console.log(`Server is running on port http://localhost:${PORT}`)
